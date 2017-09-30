b8e2c66e94096c48470b13c9a51b6003e00aa1eb
dvs23 <dvs23@dvs-board.de>
2017-09-30 21:48:52 +0200

    Queue added

--- sampleplayer/src/de/adesso/brainysnake/sampleplayer/YourPlayer.java
+++ sampleplayer/src/de/adesso/brainysnake/sampleplayer/YourPlayer.java
@@ -1,29 +1,34 @@
 package de.adesso.brainysnake.sampleplayer;
 
-import de.adesso.brainysnake.playercommon.BrainySnakePlayer;
-import de.adesso.brainysnake.playercommon.PlayerState;
-import de.adesso.brainysnake.playercommon.PlayerUpdate;
+import java.util.List;
+import java.util.Queue;
+import java.io.*;
 
+import de.adesso.brainysnake.playercommon.*;
 
 /**
  * Implementiere hier deine Schlangensteuerung.
  */
 public class YourPlayer implements BrainySnakePlayer {
-    private static int num = 0;
-    private String name;
 
-    private PlayerState playerState;
-    public YourPlayer(){
-        this.name= String.format("YourPlayer%d", ++num);
-    }
+    private PlayerState ps;
+
+    public enum doit{
+        RIGHT,
+        LEFT,
+        FORWARD
+    };
+
+    private Queue<doit> stepQ;
+
     @Override
     public String getPlayerName() {
-        return this.name;
+        return "YourName";
     }
 
     @Override
     public boolean handlePlayerStatusUpdate(PlayerState playerState) {
-        this.playerState = playerState;
+        this.ps = playerState;
         return true;
     }
 
@@ -36,6 +41,74 @@ public class YourPlayer implements BrainySnakePlayer {
 
     @Override
     public PlayerUpdate tellPlayerUpdate() {
+        if(isPoint()>-1){
+            System.out.println("Yay");
+        }
+        for(int i = 0; i<25; i++){
+            if(i%5==0)
+                System.out.println("");
+            System.out.print(printField(i));
+        }
+        System.out.println("");
+
         return new PlayerUpdate(null);
+    }
+
+    private int isPoint(){
+        int num = 0;
+        for(Field f : this.ps.getPlayerView().getVisibleFields()) {
+            if(f.getFieldType() == FieldType.POINT)
+                return num;
+        }
+        return -1;
+    }
+
+    private Orientation right(){
+        switch (ps.getPlayerView().getCurrentOrientation()){
+            case UP:
+                return Orientation.RIGHT;
+            case RIGHT:
+                return Orientation.DOWN;
+            case DOWN:
+                return Orientation.LEFT;
+            case LEFT:
+                return Orientation.UP;
+        }
+        return ps.getPlayerView().getCurrentOrientation();
+    }
+
+    private Orientation left(){
+        switch (ps.getPlayerView().getCurrentOrientation()){
+            case UP:
+                return Orientation.LEFT;
+            case RIGHT:
+                return Orientation.UP;
+            case DOWN:
+                return Orientation.RIGHT;
+            case LEFT:
+                return Orientation.DOWN;
+        }
+        return ps.getPlayerView().getCurrentOrientation();
+    }
+
+    private Orientation forward(){
+        return ps.getPlayerView().getCurrentOrientation();
+    }
+
+
+    private String printField(int id){
+        FieldType f = this.ps.getPlayerView().getVisibleFields().get(id).getFieldType();
+        switch (f) {
+            case LEVEL:
+                return "L";
+            case EMPTY:
+                return "E";
+            case PLAYER:
+                return "S";
+            case POINT:
+                return "P";
+            default:
+                return "N";
+        }
     }
 }
